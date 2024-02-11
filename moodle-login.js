24337a70a819730252c318591ae9d9f77d6a1636
marcofpadeiro <marcofpadeiro@gmail.com>
2024-02-11 13:53:59 +0000

    Inital commit

---
+++ moodle-login.js
@@ -0,0 +1,10 @@
+const usernameTB = document.getElementById("username")
+const passwordTB = document.getElementById("password")
+const loginBTN = document.getElementById("loginbtn")
+
+usernameTB.value = ""
+passwordTB.value = ""
+
+loginBTN.click()
+
+
